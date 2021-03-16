8d94f1061b1d11879418d5376b11c8b0074e3531
Tegar karunia ilham_E41200988 <74761932+Tegar-dev@users.noreply.github.com>
2021-03-16 18:47:12 +0700

    person.java

---
+++ Person.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author USER
+ */
+public class Person {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        string fName,lname,stuld,stuStatus;
+       
+        fName = "tegar karunia ilahm";
+        lname = "Tegar";
+        stuld = "E41200988";
+        stuStatus = "aktive";
+        
+//
+         System.out.println("student name\t: " + fName + " " + lname);
+        
+                
+        
+        
+        
+  
