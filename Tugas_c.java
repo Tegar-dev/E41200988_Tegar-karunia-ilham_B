6fe073cdbea9c93054b8bc7194baa4c98220eb79
Tegar karunia ilham_E41200988 <74761932+Tegar-dev@users.noreply.github.com>
2021-03-16 19:08:49 +0700

    problem.variabel.java

---
+++ Tugas_c.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_c;
+
+/**
+ *
+ * @author USER
+ */
+public class Tugas_c {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      boolean gameOver= false;
+      int student=50,classes=3;
+      double sales_tax;
+      short number1;
+      
+
+//deklarasi penulisan salah
+      int 2beOrNot2be;
+      float price index;
+      double lastyear'sPrice;
+      long class;
+    }
+    
+}
