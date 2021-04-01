201c380b08ff459d3b7c496273d6325b8ca77cf8
Tegar karunia ilham_E41200988 <74761932+Tegar-dev@users.noreply.github.com>
2021-04-01 06:19:20 -0700

    Latihan_pertemuan4_for

---
+++ Latihan_Statement_for.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_statement_for;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan_Statement_for {
+
+ 
+public static void main(String[] args) {
+      for (int i = 0; i <= 5; i++) {
+           for (int j = 0; j <= i; j++) {
+                 System.out.print("*");
+            }
+              System.out.println("");
+           }
+      }
+}              
