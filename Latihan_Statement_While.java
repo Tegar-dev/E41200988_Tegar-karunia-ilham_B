7c21d62018356c58b4b908df5f84d0cdccc35ba1
Tegar karunia ilham_E41200988 <74761932+Tegar-dev@users.noreply.github.com>
2021-04-01 06:26:11 -0700

    Latihan_pertemuan4_while

---
+++ Latihan_Statement_While.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_statement_while;
+
+/**
+ *
+ * @author user
+ */
+public class Latihan_Statement_While {
+
+
+public static void main(String[] args) {
+int i = 0;
+while (i <= 10) {
+// blok kode yang akan diulang
+System.out.println("Perulangan ke-" + i);
+// increment nilai i
+i++;
+              }
+      }
+}
