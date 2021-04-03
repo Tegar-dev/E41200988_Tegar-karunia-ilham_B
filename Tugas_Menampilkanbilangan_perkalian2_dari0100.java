a78396f7c1f1ade44b7c6b540e8f73914840be7f
Tegar karunia ilham_E41200988 <74761932+Tegar-dev@users.noreply.github.com>
2021-04-02 17:35:59 -0700

    Tugas_mingguke4_menampilkanperkalian2_0-100

---
+++ Tugas_Menampilkanbilangan_perkalian2_dari0100.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_menampilkanbilangan_perkalian2._dari.pkg0.pkg100;
+
+/**
+ *
+ * @author user
+ */
+public class Tugas_Menampilkanbilangan_perkalian2_dari0100 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    System.out.println("            Do_While" );
+    System.out.println("     bilangan perkalian 2(1-100)" );
+    System.out.println("======================================" );
+int i = 1;
+         int angka = 1;
+         do {
+             angka *= 2;
+             System.out.print(angka + " ");
+             i++;
+         } while (i<=6);
+         
+         System.out.println("" );
+    
+}
+}
