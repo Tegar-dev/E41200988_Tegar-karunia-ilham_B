69471c2403f559f1a90dd23935e5f8c5e218bd29
Tegar karunia ilham_E41200988 <74761932+Tegar-dev@users.noreply.github.com>
2021-04-02 17:34:07 -0700

    Tugas_mingguke4_menampilkanbilagankelipatan2_2-100

---
+++ Tugas_menampilkanbilangan_kelipatan2Dari0100.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_menampilkanbilangan_kelipatan2.dari.pkg0.pkg100;
+
+/**
+ *
+ * @author user
+ */
+public class Tugas_menampilkanbilangan_kelipatan2Dari0100 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    System.out.println("            for " );
+    System.out.println("     bilangan kelipatan 2(1-100)" );
+    System.out.println("======================================" );
+        for (int i=1; i<=100;i++)
+      
+        if (i % 2 == 0){
+            System.out.print(i + " ");
+            
+        }
+        }
+}
+    
+
